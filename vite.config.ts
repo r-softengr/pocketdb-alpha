17b805986f8134485f5a2000051c6f10d1bcee2e
Rick Ryan Alinsangao <alinsangao.rickryan@gmail.com>
2024-01-03 03:32:21 +0800

    heheh

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env": process.env,
-    VITE_POCKETDBBASE: process.env.VITE_POCKETDBBASE,
-  },
+// export default defineConfig(
+//   const env = loadEnv(mode, process.cwd(), '')
+//   return {
+//   plugins: [react()],
+//   define: {
+//     __APP_ENV__: JSON.stringify(env.APP_ENV),
+//   },
+// });
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
